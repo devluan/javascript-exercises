1ec4fbec70179c6b4b3c8aadc9c1628e2fe4020e
Luan Ferreira <181789192+devluan@users.noreply.github.com>
2025-09-05 12:37:18 -0300

    feat: add solution for 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,25 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((total, value) => total + value, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((total, value) => total * value, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	return new Array(n).fill().reduce((acc, cur, idx) => acc * (idx + 1), 1);
 };
 
 // Do not edit below this line
