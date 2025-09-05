dbc57db3f8b817a4fdb8bbadc3168319ff4cdf10
Luan Ferreira <181789192+devluan@users.noreply.github.com>
2025-09-05 12:57:03 -0300

    feat: add solution for 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    const sequence = [0, 1];
+    while (sequence.length <= n) {
+        sequence.push(sequence.at(-1) + sequence.at(-2));
+    }
+    return sequence.at(n);
 };
 
 // Do not edit below this line
